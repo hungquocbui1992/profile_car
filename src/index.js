4725e0faf022921fc467959eb2acb67b20c5d79d
hungquocbui1992 <buiquochung623@gmail.com>
2022-03-11 22:07:20 +0700

    nộp bài profile_car
    ok

--- src/index.js
+++ src/index.js
@@ -15,3 +15,37 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+<div className="container">
+    <div className="card">
+      <div className="card--header" />
+      <img className="avatar" src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="avatar" />
+      <div className="card--body">
+        <div>
+          <p className="text-header">Ruma Khan</p>
+          <p className="text-sub">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry
+          </p>
+          <button className="btn third" >FOLLOW</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+ReactDOM.render(
+  <div className="container">
+    <div className="card">
+      <div className="card--header" />
+      <img className="avatar" src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="avatar" />
+      <div className="card--body">
+        <div>
+          <p className="text-header">Ruma Khan</p>
+          <p className="text-sub">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry
+          </p>
+          <button className="btn" >FOLLOW</button>
+        </div>
+      </div>
+    </div>
+  </div>,
+document.getElementById('root')
+);
